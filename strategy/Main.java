73fbdc3e7fe66f0557a20995b7df7e66b8c98d52
Michel Lopes Nascimento <michel.lopes.n@gmail.com>
2023-04-23 12:23:51 -0300

    .

--- strategy/Main.java
+++ strategy/Main.java
@@ -3,24 +3,18 @@ package strategy;
 public class Main {
     public static void main(String[] args) {
         
-    Disciplina d = new Disciplina();
-    Aritmetica a = new Aritmetica();
-    Geometrica g = new Geometrica();
+        Disciplina d = new Disciplina();
+        Aritmetica a = new Aritmetica();
+        Geometrica g = new Geometrica();
 
-    d.setNome("Padroes de Desenvolvimento");
-    d.setP1(10); 
-    d.setP2(5);
-    d.setCalcMedia(g);
-    
-    /*System.out.println("Nome: " +  d.getNome());
-    System.out.println("P1: " +  d.getP1());
-    System.out.println("P2: " + d.getP2());
-    System.out.printf("Media: %.2f \n", d.calculaMedia()); 
-    System.out.println("Situacao: " + d.getSituacao()); */
+        d.setNome("Padroes de Desenvolvimento");
+        d.setP1(10); 
+        d.setP2(5);
+        d.setCalcMedia(g);    
 
-System.out.println(
-    String.format("P1:%.2f P2:%.2f MEDIA:%.2f Situacao: %s",
-    d.getP1(), d.getP2(), d.getcalculaMedia(), d.getSituacao())
-);
+        System.out.println(
+            String.format("P1:%.2f P2:%.2f MEDIA:%.2f Situacao: %s",
+            d.getP1(), d.getP2(), d.getcalculaMedia(), d.getSituacao())
+        );
     }    
 }
